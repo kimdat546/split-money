b8a22604cacaf6ae9ae7d32c24e78cb5664a9251
kimdat546 <kimdat546@gmail.com>
2022-07-25 07:24:34 +0700

    update: config path for vite

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import MainLayout from "../layouts/MainLayout";
-import { Login } from "../components";
+import { Login } from "~/components";
 
 const LoginPage = () => {
     return (
